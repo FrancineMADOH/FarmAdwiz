246f3e96ba713fdc4a9e5b69be91de6dac01c738
séraphin-git <seraphingambo697@gmail.com>
2022-03-08 04:22:32 +0100

    config

--- djfinal/blog/models.py
+++ djfinal/blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Regions(model.models):
+
+    libelle = models.CharField(max_length=150)
+    def __str__(self):
+        return self.libelle 
+
+class Posts(models.Model):
+    
+    region = models.ForeignKey(Regions,on_delete=models.CASCADE)
+    titre = models.CharField(max_length=255)
+    description = models.TextField(bank=True,null=True)
+    #image = models.ImageField(default='avatar.jpg', upload_to ='profile_Images')
+    #videos = models.FileField(upload_to='videos/', null=True, verbose_name="")
+    date = models.DateField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.titre
+
+class Comments(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts,on_delete=models.CASCADE)
+    titre = models.TextField() 
+    time = models.DateField(auto_now_add=True)
+    #image = models.ImageField(default='avatar.jpg', upload_to ='profile_Images')
+    #videos = models.FileField(upload_to='videos/', null=True, verbose_name="")
+
+
+class Reponses(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comments,on_delete=models.CASCADE)
+    titre = models.TextField()
+    time = models.DateField(auto_now_add=True)
+
+
+class Likes(models.Model):
+    post = models. ForeignKey(Posts,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE
+    like = models.BooleanField(default=False)
+
--- djfinal/project/settings.py
+++ djfinal/project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FARMADWIZ',
+        'USER': 'Adwiz',
+        'PASSWORD': 'admin123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
