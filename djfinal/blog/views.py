2abb773d20ab3cc060a936c87cfd0c30a985e023
séraphin-git <seraphingambo697@gmail.com>
2022-03-09 14:22:36 +0100

    serializer

--- djfinal/blog/views.py
+++ djfinal/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class PostsView(APIView):
+    def get(self,request):
+        query = Posts.objects.all()
+        serializer = PostsSerializer(query, many=True)
+        return Response(serializer.data) 
+
+
