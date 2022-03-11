d937bfee527458d6874c6eb4f43fc31d4bd27140
séraphin-git <seraphingambo697@gmail.com>
2022-03-11 14:57:42 +0100

    route

--- flutter/farmadwiz/lib/screens/home_screens.dart
+++ flutter/farmadwiz/lib/screens/home_screens.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
 
-class HomeScreens extends StatelessWidget {
+class HomeScreens extends StatefulWidget {
   static const routName = '/home-screens';
+
+  @override
+  State<HomeScreens> createState() => _HomeScreensState();
+}
+
+class _HomeScreensState extends State<HomeScreens> {
+
+  bool init = true;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
